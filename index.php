0a9454f64b053797d9f5ccbaff403aaf027f3f89
butaw45 <kumantheas@gmail.com>
2021-03-26 11:38:42 +0700

    Membuat tampilan sensor

--- index.php
+++ index.php
@@ -8,11 +8,72 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>IoT Sensor</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <div class="container" style="text-align: Center; margin-top:200px "> 
+    
+      <h2>Monitoring Sensor DHT11 & Soil <br>Secara Real Time</h2>
+
+      <div style="display: flex;">
+       <!-- //!Menampilkan Sensor Suhu  -->
+     <div class="card text-center" style="width:33.33%">
+      <div class="card-header" style="font-size: 30px; font-weight:bold; background-color: yellow">
+      Suhu
+      </div>
+      <div class="card-body">
+       <h1>0</h1>
+      </div>
+      
+     </div>
+      <!-- //!Akhir Menampilkan Sensor -->
+
+        <!-- //?Menampilkan Sensor Humidity  -->
+        <div class="card text-center" style="width:33.33%">
+      <div class="card-header" style="font-size: 30px; font-weight:bold; background-color: blue">
+      Humidity
+      </div>
+      <div class="card-body">
+       <h1>0</h1>
+      </div>
+      
+     </div>
+      <!-- //?Akhir Menampilkan Sensor Humidity -->
+
+        <!-- //*Menampilkan Sensor Soil  -->
+        <div class="card text-center" style="width:33.33%">
+      <div class="card-header" style="font-size: 30px; font-weight:bold; background-color: green ">
+      Soil
+      </div>
+      <div class="card-body">
+       <h1>0</h1>
+      </div>
+      
+     </div>
+      <!-- //*Akhir Menampilkan Sensor Soil -->
+      </div>
+
+    </div>
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
