3e892ce43b1603254b599729c0ff847fe35f3919
butaw45 <kumantheas@gmail.com>
2021-03-26 19:13:49 +0700

    Tambah sql DESC

--- ceksuhu.php
+++ ceksuhu.php
@@ -4,7 +4,7 @@
          $konek = mysqli_connect("localhost","root","","dbiotsensor");
 
         //! baca data tabel tb_sensor
-         $sql = mysqli_query($konek,"SELECT * FROM tb_sensor ORDER BY id");  //!data terakhir ada berada diatas
+         $sql = mysqli_query($konek,"SELECT * FROM tb_sensor ORDER BY id DESC");  //!data terakhir ada berada diatas
         //  $result = mysqli_query($konek,$sql);
 
         //!baca data paling atas
