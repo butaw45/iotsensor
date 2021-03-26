b098692aa66ce2542ca55d9af9cba445d148e8ae
butaw45 <kumantheas@gmail.com>
2021-03-26 12:02:26 +0700

    menambahkan fungsi suhu realtime

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
+    <script type="text/javascript" src="jquery/jquery.min.js"></script>
+
+    <!-- //* load otomatis /realtime -->
+    <script type="text/javascript">
+      $(document).ready( function() {
+        
+        setInterval( function () {
+          $("#ceksuhu").load("ceksuhu.php");
+
+        },1000  );
+        
+
+      } )
+    </script>
+
     <title>IoT Sensor</title>
   </head>
   <body>
@@ -23,7 +38,7 @@
       Suhu
       </div>
       <div class="card-body">
-       <h1>0</h1>
+       <h1> <span id="ceksuhu"> 0 </span> </h1>
       </div>
       
      </div>
@@ -35,7 +50,7 @@
       Humidity
       </div>
       <div class="card-body">
-       <h1>0</h1>
+       <h1> <span id="cekhumidity"> 0 </span> </h1>
       </div>
       
      </div>
@@ -47,7 +62,7 @@
       Soil
       </div>
       <div class="card-body">
-       <h1>0</h1>
+       <h1> <span id="ceksoil"> 0 </span> </h1>
       </div>
       
      </div>
